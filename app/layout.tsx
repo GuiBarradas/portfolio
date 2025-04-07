6c33066f9801074acacd491444507d672fb9de81
Guilherme Barradas <guilhermebarradasdev@gmail.com>
2025-04-07 09:22:22 -0300

    minecraft font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,32 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type React from "react"
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import { Press_Start_2P, VT323, Silkscreen } from "next/font/google"
+import "./globals.css"
+
+
+const inter = Inter({ subsets: ["latin"], display: "swap", variable: "--font-inter" })
+
+const pressStart2P = Press_Start_2P({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-press-start-2p",
+})
+
+const vt323 = VT323({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-vt323",
+})
+
+const silkscreen = Silkscreen({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-silkscreen",
+})
 
 export const metadata: Metadata = {
   title: 'hello, human',
@@ -13,6 +40,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <body className={`${inter.variable} ${pressStart2P.variable} ${vt323.variable} ${silkscreen.variable}`}></body>
       <body>{children}</body>
     </html>
   )
